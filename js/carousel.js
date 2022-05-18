e92d22449402d9c2493d7447415e0c8e1bb79ce3
chasju <91468816+chasju@users.noreply.github.com>
2022-05-18 12:23:31 +0200

    contact form validation

--- js/carousel.js
+++ js/carousel.js
@@ -1,45 +1,66 @@
-const carouselContainer = document.querySelector(".carousel_images");
+const carouselContainer = document.querySelectorAll(".carousel_images");
+const secondCarouselContainer = document.querySelectorAll(".carousel_images-mobile a");
 const carouselImage = document.querySelectorAll(".carousel_images a");
 const nextButton = document.querySelector(".right_arrow");
 const prevButton = document.querySelector(".left_arrow");
 
-let counter = 0;
-const size = carouselImage[0].clientWidth;
-const containerHeight = carouselContainer.clientHeight;
-const containerWidth = carouselContainer.clientWidth;
-console.log(containerHeight);
-console.log(carouselImage.length);
+// Re-calculating slide width
 
-carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+window.onresize = () => {
+  if (window.innerWidth > 880 && window.innerWidth < 920) {
+    window.location.reload();
+  }
+};
+
+let counter = 0;
+// Finding slide width for mobile
+
+const size = carouselImage[0].clientWidth;
+
+// Finding slide width for desktop
+
+const containerWidth = carouselContainer[0].clientWidth;
+
+// Next slide
 
 if (window.innerWidth >= "900") {
   nextButton.addEventListener("click", () => {
-    if (counter >= carouselImage.length - 4) return;
+    if (counter >= carouselContainer.length - 1) return;
 
     counter++;
-    carouselContainer.style.transform = "translateX(" + -containerWidth * counter + "px)";
+    carouselContainer.forEach((item) => {
+      item.style.transform = "translateX(" + -containerWidth * counter + "px)";
+    });
   });
 } else {
   nextButton.addEventListener("click", () => {
-    if (counter >= carouselImage.length - 1) return;
+    if (counter >= carouselImage.length - secondCarouselContainer.length - 1) return;
 
     counter++;
-    carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+    carouselContainer.forEach((item) => {
+      item.style.transform = "translateX(" + -size * counter + "px)";
+    });
   });
 }
 
+// Previous slide
+
 if (window.innerWidth >= "900") {
   prevButton.addEventListener("click", () => {
     if (counter <= 0) return;
 
     counter--;
-    carouselContainer.style.transform = "translateX(" + -containerWidth * counter + "px)";
+    carouselContainer.forEach((item) => {
+      item.style.transform = "translateX(" + -containerWidth * counter + "px)";
+    });
   });
 } else {
   prevButton.addEventListener("click", () => {
     if (counter <= 0) return;
 
     counter--;
-    carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+    carouselContainer.forEach((item) => {
+      item.style.transform = "translateX(" + -size * counter + "px)";
+    });
   });
 }
--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,72 @@
 const form = document.querySelector("form");
+const fullName = document.querySelector(".fullName");
+const errorFullName = document.querySelector(".error_name");
+const email = document.querySelector(".email");
+const errorEmail = document.querySelector(".error_email");
+const subject = document.querySelector(".subject");
+const errorSubject = document.querySelector(".error_subject");
+const textarea = document.querySelector(".textarea");
+const errorTextarea = document.querySelector(".error_textarea");
 const sendButton = document.querySelector(".contact_button");
+const thankYouMessage = document.querySelector(".thankyou_message");
 
-form.addEventListener("submit", (e) => {
+let formValid = true;
+
+function formSuccess(e) {
   e.preventDefault();
-});
+
+  formValid = true;
+
+  if (checkLength(fullName.value, 4)) {
+    errorFullName.style.display = "none";
+  } else {
+    formValid = false;
+    thankYouMessage.style.display = "none";
+    errorFullName.style.display = "block";
+  }
+
+  if (validateEmail(email.value)) {
+    errorEmail.style.display = "none";
+  } else {
+    formValid = false;
+    thankYouMessage.style.display = "none";
+    errorEmail.style.display = "block";
+  }
+
+  if (checkLength(subject.value, 14)) {
+    errorSubject.style.display = "none";
+  } else {
+    formValid = false;
+    thankYouMessage.style.display = "none";
+    errorSubject.style.display = "block";
+  }
+
+  if (checkLength(textarea.value, 24)) {
+    errorTextarea.style.display = "none";
+  } else {
+    formValid = false;
+    thankYouMessage.style.display = "none";
+    errorTextarea.style.display = "block";
+  }
+
+  if (formValid) {
+    form.remove();
+    thankYouMessage.style.display = "block";
+  }
+}
+
+function checkLength(value, length) {
+  if (value.trim().length > length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
+
+form.addEventListener("submit", formSuccess);
