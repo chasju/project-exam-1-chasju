df410d48d23739b63df86d3cacac792767e864b7
chasju <91468816+chasju@users.noreply.github.com>
2022-05-21 15:26:35 +0200

    api call carousel

--- js/api.js
+++ js/api.js
@@ -1,27 +1,166 @@
 // fetching posts
 
-const baseUrl = "http://chasju.online/allthingsjupiter/wp-json/wp/v2/posts?acf_format=standard";
-const containerImage = document.querySelector(".test");
-const container = document.querySelector(".carousel_container");
+const baseUrl =
+  "http://chasju.online/allthingsjupiter/wp-json/wp/v2/posts?acf_format=standard&per_page=20";
+const containerImages = document.querySelector(".carousel_images");
 
-async function getPosts(url) {
-  try {
-    const response = await fetch(url);
-    const posts = await response.json();
-    console.log(posts);
-
-    for (let i = 0; i < posts.length; i++) {
-      container.innerHTML += `<a href="/blogpage.html"
-                ><div class="latest_image" style="background-image: url(${posts[i].acf.header_image})"></div>
-                <h3>Example</h3></a
-              >`;
-    }
-  } catch (error) {
-    console.log(error);
+window.onresize = () => {
+  if (window.innerWidth > 880 && window.innerWidth < 920) {
+    window.location.reload();
   }
+};
+
+// Window width less than 900
+
+if (window.innerWidth <= "900") {
+  async function getPosts(url) {
+    try {
+      const response = await fetch(url);
+      const posts = await response.json();
+
+      for (let i = 0; i < posts.length; i++) {
+        if (i === 4) {
+          break;
+        }
+
+        containerImages.innerHTML += `<a href="/blogpage.html"
+                ><div class="latest_image" style="background-image: url(${posts[i].acf.header_image})"></div>
+                <h3>${posts[i].acf.headline}</h3></a
+              >`;
+      }
+
+      const carouselContainer = document.querySelectorAll(".carousel_images");
+      const secondCarouselContainer = document.querySelectorAll(".carousel_images-mobile a");
+      const carouselImage = document.querySelectorAll(".carousel_images a");
+      const nextButton = document.querySelector(".right_arrow");
+      const prevButton = document.querySelector(".left_arrow");
+
+      let counter = 0;
+
+      // Finding slide width for mobile
+
+      const size = carouselImage[0].clientWidth;
+
+      // Next slide
+      nextButton.addEventListener("click", () => {
+        if (counter >= carouselImage.length - secondCarouselContainer.length - 1) return;
+
+        counter++;
+        carouselContainer.forEach((item) => {
+          item.style.transform = "translateX(" + -size * counter + "px)";
+        });
+      });
+
+      // Previous slide
+
+      prevButton.addEventListener("click", () => {
+        if (counter <= 0) return;
+
+        counter--;
+        carouselContainer.forEach((item) => {
+          item.style.transform = "translateX(" + -size * counter + "px)";
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getPosts(baseUrl);
 }
 
-getPosts(baseUrl);
+// Window width desktop
+
+if (window.innerWidth >= "900") {
+  async function getPosts(url) {
+    try {
+      const response = await fetch(url);
+      const posts = await response.json();
+
+      for (let i = 0; i < posts.length; i++) {
+        if (i === 4) {
+          break;
+        }
+
+        containerImages.innerHTML += `<a href="/blogpage.html"
+                ><div class="latest_image" style="background-image: url(${posts[i].acf.header_image})"></div>
+                <h3>${posts[i].acf.headline}</h3></a
+              >`;
+      }
+
+      const containerImagesDesktop = document.querySelector(".carousel_images-desktop");
+
+      for (let i = 0; i < posts.length; i++) {
+        if (i < 4) {
+          continue;
+        }
+        if (i === 8) {
+          break;
+        }
+
+        containerImagesDesktop.innerHTML += `<a href="/blogpage.html"
+                ><div class="latest_image" style="background-image: url(${posts[i].acf.header_image})"></div>
+                <h3>${posts[i].acf.headline}</h3></a
+              >`;
+      }
+
+      const containerImagesDesktop2 = document.querySelector(".carousel_images-desktop2");
+
+      for (let i = 0; i < posts.length; i++) {
+        if (i < 8) {
+          continue;
+        }
+        if (i === 12) {
+          break;
+        }
+
+        containerImagesDesktop2.innerHTML += `<a href="/blogpage.html"
+                ><div class="latest_image" style="background-image: url(${posts[i].acf.header_image})"></div>
+                <h3>${posts[i].acf.headline}</h3></a
+              >`;
+      }
+
+      const carouselContainer = document.querySelectorAll(".carousel_images");
+      const carouselImage = document.querySelectorAll(".carousel_images a");
+      const nextButton = document.querySelector(".right_arrow");
+      const prevButton = document.querySelector(".left_arrow");
+
+      let counter = 0;
+
+      // Finding slide width for desktop
+
+      const containerWidth = carouselContainer[0].clientWidth;
+
+      // Next slide
+
+      console.log(carouselImage.length);
+
+      nextButton.addEventListener("click", () => {
+        if (counter >= carouselContainer.length - 1) return;
+
+        counter++;
+        carouselContainer.forEach((item) => {
+          item.style.transform = "translateX(" + -containerWidth * counter + "px)";
+        });
+      });
+
+      // Previous slide
+
+      prevButton.addEventListener("click", () => {
+        if (counter <= 0) return;
+
+        counter--;
+        carouselContainer.forEach((item) => {
+          item.style.transform = "translateX(" + -containerWidth * counter + "px)";
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getPosts(baseUrl);
+}
 
 // perPage.onchange = (event)=> {
 // const newUrl = baseUrl + `?per_page=${event.target.value}`;
